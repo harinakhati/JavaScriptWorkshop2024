3c09d3993835351c5e0089280e3099151fbaecd1
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-09-03 21:08:43 +0545

    First Commit

---
+++ day1.js
@@ -0,0 +1,32 @@
+// const secret="hello"
+// var we="welcome"
+
+
+// if(true){
+//     var name="Harina,"//var is acesible to outof the block
+//     console.log(name,"This is first one.")//const and let is not accesible out of the block
+// }
+// console.log(name,"This is second one.")
+
+// const age=18
+// const isNepal=true//no need for quotationmarks
+
+
+// let name="Harina"
+// // name=true//as 1
+// console.log(name + 18)
+
+// let friend1="Utshah"
+// let friend2="Luna"
+// let friend3="Puja"
+
+let friends=['Utshah','Luna','Puja',12,true]
+// console.log(friends)
+
+let khota={
+    institutename : "Digital Pathsala",
+    reactcourseprice : 999
+}
+console.log(khota.reactcourseprice)
+//or console.log(khota[reactcourseprice])
+// key: value
