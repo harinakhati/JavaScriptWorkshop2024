4a8568c62bc06530451e51164fe1a5d6c983cdb5
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-09-04 17:34:03 +0545

    index.html

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello, this is script.js file.")
+// console.log("Hello, this is script.js file.")
+
+const h1=document.querySelector('.thisish1')
+// // h1.textContent="Bye World"
+// h1.innerHTML="<mark>Bye World</mark>"
+// console.log(h1)
+
+const button=document.getElementsByTagName('button')[0]
+// button.textContent="Hello"
+// console.log(button)
+button.addEventListener('click',()=>{
+    h1.textContent = "Bye World"
+    var newparagraph=document.createElement('p')
+    newparagraph.textContent="Learning DOM"
+    document.body.appendChild(newparagraph)
+})
+
+
